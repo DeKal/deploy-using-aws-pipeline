fb25e11f92dc73ccf0b889e6120d77b9f0c7a1d7
Phat Ho <hhphat@apcs.vn>
2020-11-27 13:39:17 +0700

    exit

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
-yarn install
-yarn start
-
-pm2 start npm --name "app" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+cd /home/ec2-user/server/
+cp -R build/. /usr/share/nginx/html/
