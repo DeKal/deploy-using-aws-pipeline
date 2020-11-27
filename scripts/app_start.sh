7b86b2dfc45e6d959555e11b4b1f0db74ab5efe8
Phat Ho <hhphat@apcs.vn>
2020-11-27 14:20:50 +0700

    change

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/server/
 
 mkdir -p /var/www
 cp -R build/. /var/www/
-cp /home/ec2-user/server/nginx/default /etc/nginx/sites-available/default
+cp /home/ec2-user/server/nginx/nginx.conf /etc/nginx/nginx.conf
